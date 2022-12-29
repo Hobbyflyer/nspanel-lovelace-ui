485799b6396f5e8a7a9c2735650f2c50fde8c2b0
Hobbyflyer <47834021+Hobbyflyer@users.noreply.github.com>
2022-12-29 22:02:58 +0100

    Update CardPower.js
    Changes for V3.0.7.1

--- FoxHome/CardPower.js
+++ FoxHome/CardPower.js
@@ -1,56 +1,63 @@
 // Description:
 //  Creates JSON String for NSPanel CardPower
 //  Author: Thorsten Böttcher (Hobbyflyer) 
-//  Date : 11.12.2022
-//
-//  { 
-//  "id": 1,                          top left 0 (down) top right 3 (down)                 
-//  "value": 3,                       value (float) 
-//  "unit": "W",                      unit
-//  "direction": "in",                in, out, both
-//  "icon": "battery-charging-60",    icon 
-//  "iconColor": 10,                  0= green 5=yellow 10=red
-//  "speed": -3                       animation speed & direction
-//  }
+//  Date : 29.12.2022
 //
 // Icons unter: https://htmlpreview.github.io/?https://github.com/jobr99/Generate-HASP-Fonts/blob/master/cheatsheet.html
-
-// define debug= true for console output 
+// entityUpd~Energiefluss~1|1~65535~~288 W~64333~~0~0 W~64333~全~0~0 W~64333~全~0~0 W~34351~~1~288 W~34351~~0~288 W~34351~~0~0 W
+// entityUpd~Energiefluss~1|1~26095~~284 W~64333~~0~0 W~64333~全~0~0 W~64333~全~0~0 W~34351~~1~284 W~34351~~0~283 W~34351~~0~0 W
+// define "Debug=true;" for console output 
 let Debug=false;
-let Demo= true;
+// define "Demo=true;" for Demodata
+// DPJSON must be defined 
+let Demo=false;
 
-// define Datapoints .... 21 - 40 müssen angepasst werden 
+// Start configuration ****************************************************************
+
+// define Datapoints for displayed Values 
 // Aliase vom Typ INFO oder direkt auf das Objekt gehen
-var Batt_DisCharge = 'alias.0.logging.Energy.PV_BAT_DisCharge.ACTUAL';     // Lade- / Entladerate des Speichers in W  Item 1(lo) Value
-var Batt_AkkuLevel = 'alias.0.logging.Energy.PV_BAT_Level.ACTUAL';         // Füllstand des Speichers in %            Item 1(lo) icon OR "null"
-var Batt_Temp      = 'alias.0.logging.Energy.PV_BAT_TEMP_Combined.ACTUAL'; // Batterietemperatur                      Item 1(lo) icon or "null"
-var Batt_Health    = ['senec.0.BMS.SOH.0','senec.0.BMS.SOH.1'];            // Array aus Gesundheitszustand der Akkus  Item 1(lo) icon or "[null]" 
-var SolarEnergy    = 'alias.0.logging.Energy.PWR-Solar.ACTUAL';            // Ertrag Solar 1                          Item 2(lm) Value
-var BKWEnergy      = 'alias.0.logging.Energy.PWR-SolarBKW.ACTUAL';         // Ertrag Solar 2                          Item 3(lu) Value
-var HouseEnergy    = 'alias.0.logging.Energy.PWR-Haus.ACTUAL';             // Hausverbrauch                           Item 4(ro) Value
-var GridEnergy     = 'alias.0.logging.Energy.PWR-Grid.ACTUAL';             // Bezug / Einspeisung Netz                Item 4(rm) Value
-var CarEnergy      = 'alias.0.logging.Energy.PWR-Car.ACTUAL';              // WallBox                                 Item 4(ru) Value
-var DPJSON         = '0_userdata.0.PVPower';                               // Datenpunkt CardPower               
+var ValueIconCentral     = null;
+var ValueIconLeftUpper   = 'alias.0.logging.Energy.PV_BAT_DisCharge.ACTUAL';     // Lade- / Entladerate des Speichers in W  Item 1(lo) Value
+var ValueIconLeftMid     = 'alias.0.logging.Energy.PWR-Solar.ACTUAL';            // Ertrag Solar 1                          Item 2(lm) Value
+var ValueIconLeftLower   = 'alias.0.logging.Energy.PWR-SolarBKW.ACTUAL';         // Ertrag Solar 2                          Item 3(lu) Value
+var ValueIconRightUpper  = 'alias.0.logging.Energy.PWR-Haus.ACTUAL';             // Hausverbrauch                           Item 4(ro) Value
+var ValueIconRightMid    = 'alias.0.logging.Energy.PWR-Grid.ACTUAL';             // Bezug / Einspeisung Netz                Item 5(rm) Value
+var ValueIconRightLower  = 'alias.0.logging.Energy.PWR-Car.ACTUAL';              // WallBox                                 Item 6(ru) Value
+
+// define Units and Maximum Values for speed calculation
+var dpValueUnit = ['W','W', 'W', 'W', 'W', 'W', 'W'];                                
+var dpValuesMax = [6000,1300, 6000, 600, 4000, 6000, 11000];                          
+// define direction, actually not used in cardPower !!
+var valueDirection = ['both','both', 'in', 'in', 'in', 'both', 'out'];             // laut WIKI nicht genutzt 
+// define Iconimages
+var iconString = ['home','battery', 'solar-power-variant', 'solar-power-variant', 'home-import-outline', 'transmission-tower', 'car']; 
+
+// define Datapoint for writing JSON String
+var DPJSON         = '0_userdata.0.PVPower';
+
+// define additional Datapoints for Batt icon change 
+var ValueIconLeftUpperExt1 = 'alias.0.logging.Energy.PV_BAT_Level.ACTUAL';         // Füllstand des Speichers in %            OR "null"
+var ValueIconLeftUpperExt2 = 'alias.0.logging.Energy.PV_BAT_TEMP_Combined.ACTUAL'; // Batterietemperatur                      OR "null"
+var ValueIconLeftUpperExt3 = ['senec.0.BMS.SOH.0','senec.0.BMS.SOH.1'];            // Array aus Gesundheitszustand der Akkus  OR "[null]" 
+
 // Für die dynamische Aktualisierung werden Datenpunkte auf Änderung überwacht
 // Hier das Array ggf. anpassen
-var watch          = [Batt_DisCharge, SolarEnergy, BKWEnergy, HouseEnergy, GridEnergy,CarEnergy]; // array mit Datempunkten die überwacht werden sollen
-var dpValueUnit = ['W', 'W', 'W', 'W', 'W', 'W'];                           // einheiten der Werte 
-var dpValuesMax = [1300, 6000, 600, 4000, 6000, 11000];                     // Maxvalues zum Berechnen der farbe des Icons    
-var valueDirection = ['both', 'in', 'in', 'in', 'both', 'out'];             // laut WIKI nicht genutzt 
-var iconString = ['battery', 'solar-power-variant', 'solar-power-variant', 'home-import-outline', 'transmission-tower', 'car']; // Initial Icons
-var CardpowerHeader ='Energiefluss';                                        // Überschrift der Card zur Identifizierung der angezeigten Seite
+var watch          = [ValueIconLeftUpper, ValueIconLeftMid, ValueIconLeftLower, ValueIconRightUpper, ValueIconRightMid,ValueIconRightLower];
+// Überschrift der Card zur Identifizierung der angezeigten Seite muss dem Header der cardPower entsprechen.
+var CardpowerHeader ='Energiefluss';                                        
 // CustomSend DataPoints for dynamic updating screen
-var CustomSend = ['mqtt.0.NSPanel.cmnd.CustomSend','mqtt.0.NSPanelOG.cmnd.CustomSend','mqtt.0.NSPanelWZ.cmnd.CustomSend'];   /// CustomSend Datenpunkte der Panesl 
-//var DPActivePage = ['0_userdata.0.NSPanel.1.ActivePage','0_userdata.0.NSPanel.2.ActivePage','0_userdata.0.NSPanel.9.ActivePage']; // note used
-    
-// ******************************************************************
-// Check Datapoints
-getState(Batt_DisCharge,handleerror);
-getState(SolarEnergy,handleerror);
-getState(BKWEnergy,handleerror);
-getState(HouseEnergy,handleerror);
-getState(GridEnergy,handleerror);
-getState(CarEnergy,handleerror);
+var ActivePage = ['0_userdata.0.NSPanel.WZ.ActivePage.heading'];
+var CustomSend = ['mqtt.0.NSPanel.cmnd.CustomSend','mqtt.0.NSPanelOG.cmnd.CustomSend','mqtt.0.NSPanelWZ.cmnd.CustomSend'];   
+
+//  end configuration******************************************************************
+
+// Check Datapoints, create Error if something is wrong *************
+getState(ValueIconLeftUpper,handleerror);
+getState(ValueIconLeftMid,handleerror);
+getState(ValueIconLeftLower,handleerror);
+getState(ValueIconRightUpper,handleerror);
+getState(ValueIconRightMid,handleerror);
+getState(ValueIconRightLower,handleerror);
 getState(DPJSON,handleerror);
        
 function handleerror (err, state) {
@@ -61,7 +68,7 @@ function handleerror (err, state) {
     console.log(err.message);
   }
 }
-// ******************************************************************
+// End check ********************************************************
 
 // color definition according to NSPanelts.ts "Dynamische Indikatoren"
 let color0 = { red:   99, green: 190, blue: 123 };
@@ -93,16 +100,17 @@ else
 {  
 on({id: watch, change: "any"}, async function (obj) {
   var iconColor=  0;
-  dpValues = [Batt_DisCharge?getState(Batt_DisCharge).val: 0,  
-              SolarEnergy?getState(SolarEnergy).val:0, 
-              BKWEnergy?getState(BKWEnergy).val:0, 
-              HouseEnergy?getState(HouseEnergy).val:0,
-              GridEnergy?getState(GridEnergy).val:0,
-              CarEnergy?getState(CarEnergy).val:0];
+  dpValues = [ValueIconRightUpper?getState(ValueIconRightUpper).val: 0,
+              ValueIconLeftUpper?getState(ValueIconLeftUpper).val: 0,  
+              ValueIconLeftMid?getState(ValueIconLeftMid).val:0, 
+              ValueIconLeftLower?getState(ValueIconLeftLower).val:0, 
+              ValueIconRightUpper?getState(ValueIconRightUpper).val:0,
+              ValueIconRightMid?getState(ValueIconRightMid).val:0,
+              ValueIconRightLower?getState(ValueIconRightLower).val:0];
   // start JSON String for outJSON
   outJSON = '[';
   // start String for dynamic update 
-  outCustomSend='entityUpd~'+CardpowerHeader+'~1|1~65535~~';
+  outCustomSend='entityUpd~'+CardpowerHeader+'~1|1';
 
   //for each icon collect values
   for (var i_index in dpValues) {
@@ -111,47 +119,47 @@ on({id: watch, change: "any"}, async function (obj) {
     var speed =Math.round( (10* dpValues[idx]) / dpValuesMax[idx] );  
     
     //Batt icon color depends on AccuLevel   
-    if (idx == 0) 
+    if (idx == 1) 
       // check healtyness of Battery 
-      if ((Batt_Temp && getState(Batt_Temp).val > 35) || (Batt_Health[0] && getState(Batt_Health[0]).val < 97) || Batt_Health[1] && getState(Batt_Health[1]).val < 97 )
+      if ((ValueIconLeftUpperExt2 && getState(ValueIconLeftUpperExt2).val > 35) || (ValueIconLeftUpperExt3[0] && getState(ValueIconLeftUpperExt3[0]).val < 97) || ValueIconLeftUpperExt3[1] && getState(ValueIconLeftUpperExt3[1]).val < 97 )
       {
-        iconString[0]="battery-heart-variant";
+        iconString[1]="battery-heart-variant";
         iconColor=10;
       }
       else
       {
         var level;
-        Batt_AkkuLevel?level = getState(Batt_AkkuLevel).val: level=50;
+        ValueIconLeftUpperExt1?level = getState(ValueIconLeftUpperExt1).val: level=50;
         iconColor = 10 - Math.round(level / 10) ;
         speed = speed * -1; //invert direction
         // change icon
-        iconString[0]="battery-alert-variant-outline";
+        iconString[1]="battery-alert-variant-outline";
         if ( level > 10)
-          iconString[0]="battery-charging-outline";
+          iconString[1]="battery-charging-outline";
         if ( level > 20)
-          iconString[0]="battery-charging-20";
+          iconString[1]="battery-charging-20";
         if (level > 40)
-          iconString[0]="battery-charging-40";
+          iconString[1]="battery-charging-40";
         if (level > 60)
-          iconString[0]="battery-charging-60";
+          iconString[1]="battery-charging-60";
         if (level > 80)
-          iconString[0]="battery-charging-80";
+          iconString[1]="battery-charging-80";
         if (level> 95)
-          iconString[0]="battery";
+          iconString[1]="battery";
       }
    
     
-    if(idx == 1 || idx == 2)
+    if(idx == 2 || idx == 3)
       {
         iconColor = 10 - Math.round((10 * dpValues[idx]) / dpValuesMax[idx]) ;    
       }
 
-    if(idx == 3)
+    if(idx == 4)
       {
         iconColor = Math.round((10 * dpValues[idx]) / dpValuesMax[idx]) ;    
       }
 
-    if (idx == 4)
+    if (idx == 5)
       {  
         speed = speed * -1; //invert direction
       }
@@ -164,14 +172,17 @@ on({id: watch, change: "any"}, async function (obj) {
              ', \"icon\": \"' + iconString[idx] +'\"' +
              ', \"iconColor\": ' + iconColor +
              ', \"speed\": ' + speed + '}';
-    if (idx < 5)
+    if (idx < 6)
          Item= Item + ','; 
     outJSON = String(outJSON) + Item;
 
     // create item for CustomSend
     var CSicon= Icons.GetIcon(iconString[i_index]) ;
     var CSiconcolor =  rgb_dec565(color[iconColor]) ;   
-    outCustomSend= outCustomSend + '~' + CSiconcolor+ '~' + CSicon +'~' + speed + '~' + parseInt(dpValues[i_index]) + ' ' + dpValueUnit[i_index];
+    if (idx==0)
+       outCustomSend= outCustomSend + '~' + CSiconcolor+ '~' + CSicon +'~' + parseInt(dpValues[i_index]) + ' ' + dpValueUnit[i_index];
+    else
+       outCustomSend= outCustomSend + '~' + CSiconcolor+ '~' + CSicon +'~' + speed + '~' + parseInt(dpValues[i_index]) + ' ' + dpValueUnit[i_index];
   }
   // write DPJSON
   outJSON = String(outJSON) + ']';
@@ -180,12 +191,10 @@ on({id: watch, change: "any"}, async function (obj) {
  
   // dynamische Aktualisierung
   // get activepage for each Panel
-  CustomSend.forEach(function (item) {
-    var activepage= JSON.stringify(getState(item));
-    // if activepage == Energie ... // hack cause ther is not Dp with activepage
-    if (activepage.includes(CardpowerHeader))
-      setState(item,outCustomSend);
-  })
+  //ActivePage.forEach(function (item) {
+  //  if(getState(item).val==CardpowerHeader)
+       setState(CustomSend[2],outCustomSend);
+  //})
       
   if (Debug) {
     console.log(outJSON);
@@ -200,10 +209,11 @@ function rgb_dec565(color) {
 };
 
 function WriteDemoData(){
-    outJSON="[{\"id\": 0,\"value\": 1300,\"unit\": \"W\",\"direction\": \"both\",\"icon\": \"battery-charging-40\",\"iconColor\": 6,\"speed\": -10},";
-    outJSON=outJSON+"{\"id\": 1,\"value\": 2000,\"unit\": \"W\",\"direction\": \"in\",\"icon\": \"solar-power-variant\",\"iconColor\": 5,\"speed\": 4},";
-    outJSON=outJSON+"{\"id\": 2,\"value\": 100,\"unit\": \"W\",\"direction\": \"in\",\"icon\": \"solar-power-variant\",\"iconColor\": 8,\"speed\": 2},";
-    outJSON=outJSON+"{\"id\": 3,\"value\": 400,\"unit\": \"W\",\"direction\": \"in\",\"icon\": \"home-import-outline\",\"iconColor\": 1,\"speed\": 1},";
-    outJSON=outJSON+"{\"id\": 4,\"value\": -400,\"unit\": \"W\",\"direction\": \"both\",\"icon\": \"transmission-tower\",\"iconColor\": 1,\"speed\": 1},";
-    outJSON=outJSON+"{\"id\": 5,\"value\": 0,\"unit\": \"W\",\"direction\": \"both\",\"icon\": \"solar-power-variant\",\"iconColor\": 0,\"speed\": 0}]";
+    outJSON="[{\"id\": 0,\"value\": 1300,\"unit\": \"\",\"direction\": \"both\",\"icon\": \"home\",\"iconColor\": 6,\"speed\": -10},";
+    outJSON=outJSON+"{\"id\": 1,\"value\": 1300,\"unit\": \"W\",\"direction\": \"both\",\"icon\": \"battery-charging-40\",\"iconColor\": 6,\"speed\": -10},";
+    outJSON=outJSON+"{\"id\": 2,\"value\": 2000,\"unit\": \"W\",\"direction\": \"in\",\"icon\": \"car\",\"iconColor\": 5,\"speed\": 4},";
+    outJSON=outJSON+"{\"id\": 3,\"value\": 100,\"unit\": \"W\",\"direction\": \"in\",\"icon\": \"car\",\"iconColor\": 8,\"speed\": 2},";
+    outJSON=outJSON+"{\"id\": 4,\"value\": 400,\"unit\": \"W\",\"direction\": \"in\",\"icon\": \"home-import-outline\",\"iconColor\": 1,\"speed\": 1},";
+    outJSON=outJSON+"{\"id\": 5,\"value\": -400,\"unit\": \"W\",\"direction\": \"both\",\"icon\": \"transmission-tower\",\"iconColor\": 1,\"speed\": 1},";
+    outJSON=outJSON+"{\"id\": 6,\"value\": 0,\"unit\": \"W\",\"direction\": \"both\",\"icon\": \"car\",\"iconColor\": 0,\"speed\": 0}]";
 }
